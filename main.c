bf622866d4627b90d63dfc12503c653761384d2e
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-11-25 00:13:56 -0800

    Allocated dyanmic arrays for option 1 & declared dynamic arrays

--- main.c
+++ main.c
@@ -1,6 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Declare dynamic arrays/vectors and global variables
+int numResources = 0;
+int numProcesses = 0;
+int* resource;
+int* available;
+int** maxclaim;
+int** allocated;
+int** need;
+char* titles[] = {"Resources", "Available", "Max Claim", "Allocated", "Need"};
+
+void prtVector(int *vector, char *title) {
+    int size = sizeof(vector) / sizeof(vector[0]);
+    printf("\n%s: \n", title);
+    printf("\t");
+    for(int i = 0; i < size; i++) {
+        printf("r%d\t", i);
+    }
+    printf("\n\t");
+    for(int i = 0; i < size; i++) {
+        printf("%d\t", vector[i]);
+    }
+    return;
+}
+
+// TODO: Finish up option 1
+void enterGraph() {
+
+    printf("\nEnter number of resources: ");
+    scanf("%d", &numResources);
+    while (getchar() != '\n');
+    
+    resource = calloc(numResources, sizeof(int));
+    if (resource == NULL) {
+        fprintf(stderr, "Fail array allocation!");
+        return;
+    }
+    available = calloc(numResources, sizeof(int));
+    if (available == NULL) {
+        fprintf(stderr, "Fail array allocation!");
+        return;
+    }
+
+    printf("\nEnter number of units for resources (r%d to r%d): ", 0, numResources-1);
+    for(int i = 0; i < numResources; i++) {
+        scanf("%d", &resource[i]);
+    }
+    while (getchar() != '\n');
+
+    prtVector(resource, titles[0]);
+    free(resource);
+    free(available);
+    return;
+}
+
 int main(void)
 {
     int choice;
@@ -18,7 +72,7 @@ int main(void)
         while (getchar() != '\n');
         switch (choice) {
             case 1:
-                printf("\nClaim graph");
+                enterGraph();
                 break;
             case 2:
                 printf("\nRequest resource");
